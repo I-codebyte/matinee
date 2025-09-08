f627c538a0a0d8bf2907d11e81b195c24ffd88d1
IGNORAMUS1 <folorunshoopeyemi123456@gmail.com>
2025-09-08 17:45:30 +0100

    adding home component

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,7 +1,27 @@
-export default function Home(){
-    return <>
-    <section className="hero">
-
-    </section>
-    </>
+export default function Home() {
+	return (
+		<>
+			<div>
+				<section className="hero">
+					<div className="l-gradient">
+						<h2>The Last Stand</h2>
+						<p>
+							A thrilling action film
+							a lone hero's fight
+							against injustice.
+							Witness epic battles and
+							a story of courage
+							against all odds
+						</p>
+						<div className="hero-action">
+							<button>Play</button>
+							<button>
+								More Info
+							</button>
+						</div>
+					</div>
+				</section>
+			</div>
+		</>
+	);
 }
